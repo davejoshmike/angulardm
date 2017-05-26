25dd69860437da0a207ef2477acfa0427dff2ff6
davejoshmike <davejoshmike@gmail.com>
2017-05-26 15:47:35 -0400

    Finishing up lab03

--- app.js
+++ app.js
@@ -2,13 +2,13 @@
   * Reads the config files and starts the express server
   */
 // var config = require('config');
+//var path = require('path');
 var express = require('express');
 var app = express();
-
-app.use(express.static('app'));
+app.use(express.static('public'));
 
 app.get('/', function(req, res) {
-  res.send("Hello");
+  res.sendFile(__dirname + '/public/index.html');S
 });
 
 // var hostname = config.util.getEnv('HOSTNAME');
@@ -16,11 +16,11 @@ app.get('/', function(req, res) {
 // console.log('Host:' + hostname);
 // console.log('Port: ' + portenv);
 
-var server = app.listen(8080, function() {
+var server = app.listen(process.env.PORT || 8080, function() {
   // var server = app.listen(process.env.PORT || 8080, function() {
   // var host = server.address().address;
   // var port = server.address().port;
   // console.log('server Host:' + host);
   // console.log('server Port: ' + port);
-  console.log("Example app listening on port: " + "8080");
+  console.log("Example app listening on port: " + server.address().port);
 });
